3826b40053cc63f77997aa675a54b4295f7b558f
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2024-03-03 20:46:05 +0100

    Add preprocessor base class

---
+++ mygrad/mygrad/preprocessors/__init__.py
@@ -0,0 +1,1 @@
+from mygrad.preprocessors.base import Preprocessor
---
+++ mygrad/mygrad/preprocessors/base.py
@@ -0,0 +1,22 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class Preprocessor(ABC):
+    input_size: int
+    output_size: int
+
+    @abstractmethod
+    def fit(self, x: np.ndarray) -> np.ndarray:
+        """Fit the preprocessor to the data"""
+        ...
+
+    @abstractmethod
+    def transform(self, x: np.ndarray) -> np.ndarray:
+        """Transform the data using the preprocessor"""
+        ...
+
+    def fit_transform(self, x: np.ndarray) -> np.ndarray:
+        """Fit the preprocessor to the data and transform it"""
+        self.fit(x)
+        return self.transform(x)
