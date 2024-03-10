613935a3e31252206fb22f7a9fc2c130b8e43e85
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2024-03-11 00:17:21 +0100

    Clean and organize code

---
+++ mygrad/mygrad/dataloaders.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+from collections.abc import Iterator
+
+import numpy as np
+from numpy.lib import math
+
+
+class Dataloader(ABC):
+    batch_size: int
+
+    @abstractmethod
+    def __len__(self) -> int:
+        ...
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[tuple[np.ndarray, np.ndarray]]:
+        ...
+
+
+class NumpyRegressionDataloader:
+    def __init__(
+        self, x: np.ndarray, y: np.ndarray, batch_size: int, shuffle: bool = False
+    ):
+        assert len(x.shape) == 2, "x must be a 2D array"
+        assert len(y.shape) == 2, "x must be a 2D array"
+        assert x.shape[0] == y.shape[0], "x and y must have the same number of samples"
+        assert y.shape[1] == 1, "y must be a 2D array with a single column"
+
+        self.x = x
+        self.y = y
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.sample_length = x.shape[0]
+
+    def __len__(self) -> int:
+        return math.ceil(len(self.x) / self.batch_size)
+
+    def __iter__(self) -> Iterator[tuple[np.ndarray, np.ndarray]]:
+        iterations = math.ceil(len(self.x) / self.batch_size)
+        for i in range(0, iterations):
+            start_index = i * self.batch_size
+            end_index = min(start_index + self.batch_size, self.sample_length)
+            yield (
+                self.x[start_index:end_index].copy(),
+                self.y[start_index:end_index].copy(),
+            )
+        if self.shuffle:
+            p = np.random.permutation(len(self.x))
+            self.x = self.x[p, :]
+            self.y = self.y[p, :]
--- mygrad/mygrad/trainers.py
+++ mygrad/mygrad/trainers.py
@@ -1,77 +1,12 @@
-from abc import ABC, abstractmethod
-from collections.abc import Iterator
 from typing import Optional
 
 import numpy as np
-from numpy.lib import math
 from tqdm.auto import tqdm
 from dataclasses import dataclass
 from mygrad.components.base import Component
+from mygrad.dataloaders import Dataloader
 from mygrad.losses import Loss
 from mygrad.optimizers.base import Optimizer
-
-
-class Dataloader(ABC):
-    batch_size: int
-
-    @abstractmethod
-    def __len__(self) -> int:
-        ...
-
-    @abstractmethod
-    def __iter__(self) -> Iterator[tuple[np.ndarray, np.ndarray]]:
-        ...
-
-
-class NumpyRegressionDataloader:
-    def __init__(
-        self, x: np.ndarray, y: np.ndarray, batch_size: int, shuffle: bool = False
-    ):
-        assert len(x.shape) == 2, "x must be a 2D array"
-        assert len(y.shape) == 2, "x must be a 2D array"
-        assert x.shape[0] == y.shape[0], "x and y must have the same number of samples"
-        assert y.shape[1] == 1, "y must be a 2D array with a single column"
-
-        self.x = x
-        self.y = y
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.sample_length = x.shape[0]
-
-    def __len__(self) -> int:
-        return math.ceil(len(self.x) / self.batch_size)
-
-    def __iter__(self) -> Iterator[tuple[np.ndarray, np.ndarray]]:
-        iterations = math.ceil(len(self.x) / self.batch_size)
-        for i in range(0, iterations):
-            start_index = i * self.batch_size
-            end_index = min(start_index + self.batch_size, self.sample_length)
-            yield (
-                self.x[start_index:end_index].copy(),
-                self.y[start_index:end_index].copy(),
-            )
-        if self.shuffle:
-            p = np.random.permutation(len(self.x))
-            self.x = self.x[p, :]
-            self.y = self.y[p, :]
-
-
-# class AbstractTrainer(ABD):
-#     @abstractmethod
-#     def train(self, train_epochs: int):
-#         ...
-#
-#     @abstractmethod
-#     @overload
-#     def eval(
-#         self, dataloader: Dataloader, return_loss: bool
-#     ) -> (np.ndarray, float):
-#         ...
-#
-#     @abstractmethod
-#     @overload
-#     def eval(self, dataloader: Dataloader, return_loss: Literal[False]) -> np.ndarray:
-#         ...`)
 
 
 @dataclass
@@ -173,54 +108,8 @@ class RegressionTrainer:
             y_pred = self.model.forward(x_batch)
             all_ys.append(y_batch)
             all_ys_pred.append(y_pred)
-        # self.model.zero_grad()
+        self.model.zero_grad()
 
         all_ys = np.concatenate(all_ys, axis=0)
         all_ys_pred = np.concatenate(all_ys_pred, axis=0)
         return all_ys, float(self.loss_func.value(all_ys_pred, all_ys))
-
-    # def test(self):
-    #     assert self.test_dataloader is not None, "No test dataloader provided"
-    #
-    #     print("### Testing")
-    #     collected_y_pred = []
-    #     collected_y = []
-    #     for x_batch, y_batch in tqdm(self.test_dataloader):
-    #         y_pred = self.model.forward(x_batch)
-    #         loss_value, loss_grad = self.loss_func.both(y_pred, y_batch)
-    #
-    #         collected_y_pred.append(y_pred)
-    #         collected_y.append(y_batch)
-    #
-    #     self.optimizer.zero_grad()
-    #
-    #     collected_y_pred = np.concatenate(collected_y_pred)
-    #     collected_y = np.concatenate(collected_y)
-    #
-    #     loss = self.loss_func.value(collected_y_pred, collected_y)
-    #     print(f"Test loss: {loss}")
-    #     return loss
-    #
-    # def validate(self) -> float:
-    #     assert (
-    #         self.validation_dataloader is not None
-    #     ), "No validation dataloader provided"
-    #
-    #     print("### Validation")
-    #     collected_y_pred = []
-    #     collected_y = []
-    #     for x_batch, y_batch in tqdm(self.validation_dataloader):
-    #         y_pred = self.model.forward(x_batch)
-    #         loss_value, loss_grad = self.loss_func.both(y_pred, y_batch)
-    #
-    #         collected_y_pred.append(y_pred)
-    #         collected_y.append(y_batch)
-    #
-    #     self.optimizer.zero_grad()
-    #
-    #     collected_y_pred = np.concatenate(collected_y_pred)
-    #     collected_y = np.concatenate(collected_y)
-    #
-    #     loss = self.loss_func.value(collected_y_pred, collected_y)
-    #     print(f"Validation loss: {loss}")
-    #     return float(loss)
