Fix mse grad calculation

mygrad/mygrad/losses.py
@@ -41,12 +41,14 @@ class MeanSquareErrorLoss(Loss):
         return ff.mse(y_pred, y_true, axis=0)
 
     def grad(self, y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
-        return 2 * (y_pred - y_true)
+        n = y_pred.shape[0]
+        return 2 * (y_pred - y_true) / n
 
 
 class LogCoshLoss(Loss):
     def value(self, y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
-        return np.log(np.cosh(y_pred - y_true)).sum()
+        return np.log(np.cosh(y_pred - y_true)).mean()
 
     def grad(self, y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
-        return np.tanh(y_pred - y_true)
+        n = y_pred.shape[0]
+        return np.tanh(y_pred - y_true) / n

mygrad/tests/test_losses.py
@@ -23,4 +23,4 @@ def test_mse_loss():
     y = np.array([[1, 2, 3], [4, 5, 6]])
     loss = losses.MeanSquareErrorLoss()
     assert np.allclose(loss.value(y_pred, y), np.array([2, 0.5, 0]))
-    assert np.allclose(loss.grad(y_pred, y), np.array([[0, 0, 0], [-4, -2, 0]]))
+    assert np.allclose(loss.grad(y_pred, y), np.array([[0, 0, 0], [-2, -1, 0]]))