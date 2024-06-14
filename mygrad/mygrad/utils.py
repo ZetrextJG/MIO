c737d3c1239e544a0377fa09d0e9dfc87648cfdf
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2024-06-14 13:59:21 +0200

    Write util fnctions to input/output param vectors

--- mygrad/mygrad/utils.py
+++ mygrad/mygrad/utils.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 from typing import Any
+import numpy as np
+
+from mygrad.components.base import Component
 
 
 def indent_lines(text: str):
@@ -20,3 +23,28 @@ def prefix_dict_keys(d: dict[str, Any], prefix: str) -> dict[str, Any]:
 
 def sort_dict(d: dict[str, Any]) -> dict[str, Any]:
     return dict(sorted(d.items()))
+
+
+def get_parameter_number(comp: Component):
+    count = 0
+    for p in comp.parameters():
+        count += p.data.size
+    return count
+
+
+def get_param_vector(comp: Component) -> np.ndarray:
+    n = get_parameter_number(comp)
+    params = np.ones(n, dtype=np.float64) * np.nan
+    current = 0
+    for p in comp.parameters():
+        params[current : current + p.data.size] = p.data.flatten()
+        current += p.data.size
+    return params
+
+
+def set_parameter_vector(comp: Component, params_vector: np.ndarray):
+    current = 0
+    for p in comp.parameters():
+        size = p.data.size
+        p.data = params_vector[current : current + size].reshape(p.data.shape)
+        current += size
--- mygrad/tests/test_utils.py
+++ mygrad/tests/test_utils.py
@@ -1,2 +1,33 @@
-def indent_lines(text: str):
-    return "\t" + text.replace("\n", "\n\t")
+import mygrad.components as mc
+import numpy as np
+from mygrad.utils import get_param_vector, get_parameter_number, set_parameter_vector
+
+
+def test_getting_param_count():
+    network = mc.Sequential(
+        mc.Linear(2, 3), mc.ReLU(), mc.Linear(3, 3), mc.ReLU(), mc.Linear(3, 1)
+    )
+    true_param_count = 6 + 3 + 9 + 3 + 3 + 1
+    param_count = get_parameter_number(network)
+    assert true_param_count == param_count
+
+
+def test_getting_param_vector():
+    network = mc.Sequential(
+        mc.Linear(2, 3), mc.ReLU(), mc.Linear(3, 3), mc.ReLU(), mc.Linear(3, 1)
+    )
+    param_vector = get_param_vector(network)
+    assert not np.isnan(param_vector).any()
+
+
+def test_mutate_param_vector():
+    network = mc.Sequential(
+        mc.Linear(2, 3), mc.ReLU(), mc.Linear(3, 3), mc.ReLU(), mc.Linear(3, 1)
+    )
+    param_vector = get_param_vector(network)
+    param_vector[0] = 100
+    set_parameter_vector(network, param_vector)
+
+    new_param_vector = get_param_vector(network)
+    assert new_param_vector[0] == 100
+    assert np.allclose(new_param_vector, param_vector)
